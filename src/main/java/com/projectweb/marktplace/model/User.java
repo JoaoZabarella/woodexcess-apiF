12944ddf3ad1624a88a6288e863c12a3736f4d3b
JoaoZabarella <zabarellaj@gmail.com>
2025-11-11 19:32:12 -0300

    fix: copilation errors correction

--- src/main/java/com/projectweb/marktplace/model/User.java
+++ src/main/java/com/projectweb/marktplace/model/User.java
@@ -1,11 +1,19 @@
 package com.projectweb.marktplace.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
     @Id
